e8ef64407d962de116069a583dbcc463c1b382a1
wangxuan2 <wangxuan2@jk.cn>
2016-09-21 16:51:17 +0800

    首次添加

---
+++ pat-elf/src/main/java/com/pawx/pat_elf/App.java
@@ -0,0 +1,13 @@
+package com.pawx.pat_elf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
