dc0784a38c3d447d503fb372ddb9e633c0dd743c
wangxuan2 <wangxuan2@jk.cn>
2016-09-22 10:21:48 +0800

    新增文件

---
+++ pat-elf/src/main/java/com/pawx/pat_elf/ApplicationMain.java
@@ -0,0 +1,17 @@
+package com.pawx.pat_elf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Domain
+ *
+ */
+@SpringBootApplication
+public class ApplicationMain 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ApplicationMain.class, args);
+    }
+}
