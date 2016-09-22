76cdca62266fa86de763b76df7f77a0694723ff8
wangxuan2 <wangxuan2@jk.cn>
2016-09-22 14:06:08 +0800

    测试图片

--- pat-elf/src/main/java/com/pawx/pat_elf/ApplicationMain.java
+++ pat-elf/src/main/java/com/pawx/pat_elf/ApplicationMain.java
@@ -2,12 +2,14 @@ package com.pawx.pat_elf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Domain
  *
  */
 @SpringBootApplication
+@ComponentScan("com.pawx")
 public class ApplicationMain 
 {
     public static void main( String[] args )
